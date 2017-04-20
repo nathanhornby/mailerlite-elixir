f0b896b85afd63ac56b8161d949c57cdadca993a
Nathan Hornby <nathan.hornby@googlemail.com>
2017-04-20 22:37:32 +0100

    Added delete_subscriber function

--- lib/groups.ex
+++ lib/groups.ex
@@ -9,8 +9,6 @@ defmodule MailerLite.Groups do
   @doc ~S"""
   Adds a new subscriber to a group.
 
-  TODO Use ELixir native `Date`
-
   [![API reference](https://img.shields.io/badge/MailerLite API-→-00a154.svg?style=flat-square)](https://developers.mailerlite.com/reference#add-single-subscriber)
 
   ## Example request
@@ -76,5 +74,49 @@ defmodule MailerLite.Groups do
     end
   end
 
-  def add_subscriber(_group, _subscriber), do: {:error, :invalid_argument}
+  def add_subscriber(_group, _new_subscriber), do: {:error, :invalid_argument}
+
+  @doc ~S"""
+  Deletes a subscriber from a group.
+
+  [![API reference](https://img.shields.io/badge/MailerLite API-→-00a154.svg?style=flat-square)](https://developers.mailerlite.com/reference#remove-subscriber)
+
+  ## Example request
+
+      MailerLite.Groups.delete_subscriber(2345678, "james.moon.fake@gmail.com")
+      MailerLite.Groups.delete_subscriber(2345678, "1343965485")
+
+  ## Example response
+
+      {:ok}
+
+  ## Tests
+
+      iex> MailerLite.Groups.delete_subscriber(6322190, "james.fakefake@googlemail.com")
+      {:ok}
+
+      iex> MailerLite.Groups.delete_subscriber(0000001, "james.fakefake@googlemail.com")
+      {:error, :not_found}
+
+      iex> MailerLite.Groups.delete_subscriber("6322190", 1343965485)
+      {:error, :invalid_argument}
+  """
+  @spec delete_subscriber(MailerLite.id, String.t) :: {:ok} | {:error, atom}
+  def delete_subscriber(group, subscriber) when is_integer(group) and is_binary(subscriber) do
+    url = @endpoint <> "/" <> Integer.to_string(group) <> "/subscribers/" <> subscriber
+    case HTTPoison.delete(url, @headers) do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
+        {:ok}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, :not_found}
+      {:ok, %HTTPoison.Response{status_code: 500}} ->
+        {:error, :server_error}
+      {_, %HTTPoison.Response{status_code: code, body: body}} ->
+        {code, body}
+      _ ->
+        {:error, :network_error}
+    end
+  end
+
+  def delete_subscriber(_group, _subscriber), do: {:error, :invalid_argument}
 end
